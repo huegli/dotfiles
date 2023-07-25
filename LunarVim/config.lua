30c179e6087c7ad97664d27a49b911ee2e5947d2
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-07-25 14:08:03 -0700

    Clean formated config file

--- LunarVim/config.lua
+++ LunarVim/config.lua
@@ -1,8 +1,4 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
-
+-- General Vim / NeoVim options
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 
@@ -14,13 +10,15 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- LunarVim Plugin's
 lvim.plugins = {
-  { "catppuccin/nvim",
+  {
+    "catppuccin/nvim",
     name = "catppuccin",
     opts = {
       flavour = "frappe",
     },
   },
-  { "max397574/better-escape.nvim",
+  {
+    "max397574/better-escape.nvim",
     config = true,
   }
 }
