787bdfd39c041c6dc967f7223512f163bb4edc3c
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-08-23 09:57:32 -0700

    VI mode with jk for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+  
+  # VI mode
+  fish_vi_key_bindings
+  # fish_user_key_bindings
+  bind --mode insert --sets-mode default jk repaint
 end
